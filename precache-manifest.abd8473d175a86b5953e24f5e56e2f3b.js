f1fd71c25ded601c002427c5085133c790652f28
Dave Collison <davecollison@BaseMac.local>
2020-06-19 18:24:07 +0100

    Updates

---
+++ precache-manifest.abd8473d175a86b5953e24f5e56e2f3b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40381baded14b473a02835a23caab82f",
+    "url": "/fcc-calculator/index.html"
+  },
+  {
+    "revision": "c0fabff6ab7d2e50a13d",
+    "url": "/fcc-calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f10fba48f7fe0c2f2ac",
+    "url": "/fcc-calculator/static/js/2.41f1c41f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fcc-calculator/static/js/2.41f1c41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0fabff6ab7d2e50a13d",
+    "url": "/fcc-calculator/static/js/main.df639ef6.chunk.js"
+  },
+  {
+    "revision": "7281f81b1ccd46a2aea4",
+    "url": "/fcc-calculator/static/js/runtime-main.ea8d039d.js"
+  },
+  {
+    "revision": "f6a5d082601eafec8aca390152bdd346",
+    "url": "/fcc-calculator/static/media/logo.f6a5d082.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.6bb2c2b761634d961c6324e8b027941b.js"
+  "/fcc-calculator/precache-manifest.abd8473d175a86b5953e24f5e56e2f3b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fcc-calculator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
