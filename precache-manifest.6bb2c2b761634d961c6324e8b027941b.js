6fa2c0d98d399b5e0de4d6c02aed5a6b0a11bfb9
Dave Collison <davecollison@BaseMac.local>
2020-06-19 18:23:21 +0100

    Updates

---
+++ precache-manifest.6bb2c2b761634d961c6324e8b027941b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81fb68b092d76090a5b422ccb65e5ece",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e5214b47655898a1c4",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f10fba48f7fe0c2f2ac",
+    "url": "/static/js/2.41f1c41f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.41f1c41f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e5214b47655898a1c4",
+    "url": "/static/js/main.3e63721c.chunk.js"
+  },
+  {
+    "revision": "411891b6d357d5e99f81",
+    "url": "/static/js/runtime-main.e1718cc5.js"
+  },
+  {
+    "revision": "f6a5d082601eafec8aca390152bdd346",
+    "url": "/static/media/logo.f6a5d082.svg"
+  }
+]);
